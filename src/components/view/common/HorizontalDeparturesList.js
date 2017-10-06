7d472e611e116d0456ca87c42aa741bea1c52079
Eirik Vikanes <eirikv@fastmail.com>
2017-10-06 19:41:52 +0200

    There are now four departures times on a screen width, and the scroll view snaps to fit these

--- src/components/view/common/HorizontalDeparturesList.js
+++ src/components/view/common/HorizontalDeparturesList.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import { FlatList, Text } from 'react-native'
+import { FlatList, Text, StyleSheet, Dimensions } from 'react-native'
 import moment from 'moment'
 import 'moment/locale/nb'
 import { text } from '../../../styles'
 
 const HorizontalDeparturesList = ({ departures }) => (
     <FlatList
-        contentContainerStyle={{ justifyContent: 'space-around', alignItems: 'center' }}
+        contentContainerStyle={styles.containerStyle}
         horizontal
-        snapToInterval={200}
+        snapToInterval={windowWidth}
         snapToAlignment={"center"}
         data={departures}
         keyExtractor={(item, index) => item.rt === 1 ? item.t : item.ts}
-        renderItem={({ item }) => <Text style={text.size.large}> {createDisplayTime(item.rt === 1 ? item.t : item.ts)} </Text>}
+        renderItem={({ item }) => <Text style={[text.size.large, styles.quarterSize]}> {createDisplayTime(item.rt === 1 ? item.t : item.ts)} </Text>}
     />
 )
+
+const windowWidth = Dimensions.get('window').width
 
 const createDisplayTime = timeString => {
     moment.locale('nb')
@@ -39,6 +41,17 @@ const createDisplayTime = timeString => {
     const departureTime = moment(timeString, 'DD.MM.YYYY HH:mm')
     return departureTime.isBefore(moment().add(15, 'm')) ? departureTime.fromNow(true) : departureTime.format('HH:mm')
 }
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    quarterSize: {
+        width: windowWidth / 4,
+        textAlign: 'center'
+    }
+})
 
 export default HorizontalDeparturesList
 
